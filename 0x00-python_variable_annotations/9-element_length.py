0036e8666f11115923066b97e851b5b987ec4c12
washucode <estherw8525@gmail.com>
2024-02-12 09:54:06 +0300

    task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3  
 """ Basic annotations - task 9 """
 
-from typing import List, Union, Tuple
+from typing import List, Union, Tuple,Sequence, Iterable
 
 
-def element_length(lst: List[Union[str, List[Union[int, float]]]]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ Returns a list of tuples, where the first element is a string and the second is an int """
     return [(i, len(i)) for i in lst]
